e4c477d6319905c206e1af7437cfe6031fa376c4
Vishal Kadam <133477889+vishalkadam9021@users.noreply.github.com>
2023-05-16 14:52:39 +0530

    Add files via upload

---
+++ DemoProg.java
@@ -0,0 +1,20 @@
+package BasicDemo;
+
+import java.util.Scanner;
+
+public class DemoProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println(" Enter number1 ");
+		a=sc.nextInt();
+		System.out.println(" Enter number2 ");
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println(" Addition = "+c);
+	}
+
+}
